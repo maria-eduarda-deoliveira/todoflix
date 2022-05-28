74f0c1d70fae0623bbd229ce6b3503db2415f416
Maria Eduarda de Oliveira Garcia <ol.mariae@outlook.com>
2022-05-28 20:39:21 -0300

    Updates

---
+++ precache-manifest.bf6bf23be925c1c9a8f08280f4585014.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = [
+  {
+    "revision": "863cf8d97873ea510700",
+    "url": "/todoflix/static/css/main.a191e2e9.chunk.css"
+  },
+  {
+    "revision": "863cf8d97873ea510700",
+    "url": "/todoflix/static/js/main.863cf8d9.chunk.js"
+  },
+  {
+    "revision": "a7427b737c776be7b064",
+    "url": "/todoflix/static/js/1.a7427b73.chunk.js"
+  },
+  {
+    "revision": "96247ffca8b12c79dd2b",
+    "url": "/todoflix/static/js/runtime~main.96247ffc.js"
+  },
+  {
+    "revision": "18e41439ca84dc99d01f8baa33c7c1c6",
+    "url": "/todoflix/static/media/Coracao.18e41439.svg"
+  },
+  {
+    "revision": "85705ac43878f87d5b55e01b1b11634d",
+    "url": "/todoflix/static/media/verde.85705ac4.svg"
+  },
+  {
+    "revision": "63fc7d0c75f9fce11725a9cf9e8cf29a",
+    "url": "/todoflix/static/media/branco.63fc7d0c.svg"
+  },
+  {
+    "revision": "3fe2ab18231fdafa4c7ac5e53545c3c3",
+    "url": "/todoflix/static/media/CoracaoBranco.3fe2ab18.svg"
+  },
+  {
+    "revision": "4929d20c0710f1898f92b47b6d8b7d5f",
+    "url": "/todoflix/static/media/Search.4929d20c.svg"
+  },
+  {
+    "revision": "762c8306917f9a51050c9c6a10f92f66",
+    "url": "/todoflix/static/media/laranja.762c8306.svg"
+  },
+  {
+    "revision": "8281984f8590a5a8c516d6ed327590ff",
+    "url": "/todoflix/static/media/Shrek.8281984f.png"
+  },
+  {
+    "revision": "1814395e8f7374f486ef6955a59f3c35",
+    "url": "/todoflix/static/media/Sozinho.1814395e.png"
+  },
+  {
+    "revision": "15d9574c6ebfdd0dd79e9e6b1326489f",
+    "url": "/todoflix/static/media/Spiderman.15d9574c.png"
+  },
+  {
+    "revision": "24504394e7372605ee3d790c55baed99",
+    "url": "/todoflix/static/media/Sonho.24504394.png"
+  },
+  {
+    "revision": "5d13ba262c36404e82538b9e84647b56",
+    "url": "/todoflix/static/media/Horas.5d13ba26.png"
+  },
+  {
+    "revision": "563d1d489d1346f3a47134067f6d2d01",
+    "url": "/todoflix/static/media/caret.563d1d48.svg"
+  },
+  {
+    "revision": "835c1a4007f9505a6aec51b41045cc1f",
+    "url": "/todoflix/static/media/profile.835c1a40.svg"
+  },
+  {
+    "revision": "9817fda53952579bee3391ca09131129",
+    "url": "/todoflix/static/media/Capitao.9817fda5.png"
+  },
+  {
+    "revision": "e81a01ebd8bd02a4ce2fe58a0d0f49cd",
+    "url": "/todoflix/static/media/Arrow.e81a01eb.svg"
+  },
+  {
+    "revision": "ff4997e701335ec4ce43638f1464e8ea",
+    "url": "/todoflix/static/media/Galinhas.ff4997e7.png"
+  },
+  {
+    "revision": "5d296399735ae8273caebd2a7ec17e11",
+    "url": "/todoflix/static/media/Rocketman.5d296399.png"
+  },
+  {
+    "revision": "3362d6271c7f8e8fd052c42aea727b9a",
+    "url": "/todoflix/static/media/Amar.3362d627.png"
+  },
+  {
+    "revision": "1f132a5e2e746e763ea245c34fe12443",
+    "url": "/todoflix/static/media/vermelho.1f132a5e.svg"
+  },
+  {
+    "revision": "811076a85547c37e245f5b0b2257a7c0",
+    "url": "/todoflix/index.html"
+  }
+];
